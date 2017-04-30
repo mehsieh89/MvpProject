96b56d0c360444ddc5b583e9f0972c20e8d53db1
Hack Reactor Student <student@hackreactor.com>
2017-04-29 17:26:30 -0700

    finished weakness object for battle algorithm

--- react-client/src/components/BattleData.js
+++ react-client/src/components/BattleData.js
@@ -24,8 +24,21 @@ export var strong = {
 weak = { 
 	normal: ['rock', 'steel', 'ghost'],
 	fire: ['fire', 'water', 'rock', 'dragon'],
-	
-
-
+	water: ['water', 'grass', 'dragon'],
+	electric: ['electric', 'grass', 'ground', 'dragon'],
+	grass: ['fire', 'grass', 'poison', 'flying', 'bug', 'dragon', 'steel'],
+	ice: ['fire', 'water', 'ice', 'steel'],
+	fighting: ['poison', 'flying', 'psychic', 'bug', 'ghost', 'fairy'],
+	poison: ['poison', 'ground', 'rock', 'ghost', 'steel'],
+	ground: ['grass', 'flying', 'bug'],
+	flying: ['electric', 'rock', 'steel'],
+	psychic: ['psychic', 'dark', 'steel'],
+	bug: ['fire', 'fighting', 'poison', 'flying', 'ghost', 'steel', 'fairy'],
+	rock: ['fighting', 'ground', 'steel'],
+	ghost: ['normal', 'dark'],
+	dragon: ['steel', 'fairy'],
+	dark: ['fighting', 'dark', 'fairy'],
+	steel: ['fire', 'water', 'electric', 'steel'],
+	fairy: ['fire', 'poison', 'steel']
 }
 
